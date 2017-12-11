72b2b4bcc21e3c20ff35bc9b1d2206836e0169fe
hharris3 <houston_harris@intuit.com>
2017-12-10 19:11:07 -0600

    Updates

--- src/utils/styled-theme.js
+++
@@ -1,4 +0,0 @@
-export const theme = {
-  headerBg: 'rebeccapurple',
-  bodyBg: '#eee'
-}
--- src/utils/typography.js
+++
@@ -1,7 +0,0 @@
-import Typography from "typography";
-import judahTheme from "typography-theme-judah";
-
-judahTheme.baseFontSize = "14px";
-const typography = new Typography(judahTheme);
-
-export default typography;
